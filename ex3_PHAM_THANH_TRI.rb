0bd451d3ece79454c4df9fc80159583b45ec1992
PThanhTri <tri.pham@rikai.technology>
2023-05-23 15:49:16 +0700

    commit ex

--- ex3_PHAM_THANH_TRI.rb
+++ ex3_PHAM_THANH_TRI.rb
@@ -1,14 +1,14 @@
 def perimeter_and_area_of_triangle(a, b, c)
-    if a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a
+                  if a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a
       puts 'Đây không phải là một tam giác hợp lệ.'
-      return
+             return
     end
   
-    perimeter = a + b + c
+             perimeter = a + b + c
     p = perimeter / 2.0
     acreage = Math.sqrt(p * (p - a) * (p - b) * (p - c))
-  
-    puts "Chu vi của tam giác là: #{perimeter}"
+   
+                puts "Chu vi của tam giác là: #{perimeter}"
     puts "Diện tích của tam giác là: #{acreage}"
   end
   
